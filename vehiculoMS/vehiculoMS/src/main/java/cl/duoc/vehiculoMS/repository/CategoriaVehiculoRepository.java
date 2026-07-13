6770a862df5a9bf045f28cb6dba2ed678a78649d
CALEB <go.gallardoe@duocuc.cl>
2026-07-12 21:24:46 -0400

    upgrate CategoriaVehiculoRepository

--- vehiculoMS/vehiculoMS/src/main/java/cl/duoc/vehiculoMS/repository/CategoriaVehiculoRepository.java
+++ vehiculoMS/vehiculoMS/src/main/java/cl/duoc/vehiculoMS/repository/CategoriaVehiculoRepository.java
@@ -1,8 +1,6 @@
 package cl.duoc.vehiculoMS.repository;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import cl.duoc.vehiculoMS.model.CategoriaVehiculo;
 
 @Repository
