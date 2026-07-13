4ec7f5aae2d979859947920e8ab9af06fa4bc5d6
CALEB <go.gallardoe@duocuc.cl>
2026-07-12 21:47:15 -0400

    upgrate

--- disponibilidadMS/disponibilidadMS/src/main/java/cl/duoc/disponibilidadMS/config/OpenApiConfig.java
+++ disponibilidadMS/disponibilidadMS/src/main/java/cl/duoc/disponibilidadMS/config/OpenApiConfig.java
@@ -1,5 +1,4 @@
 package cl.duoc.disponibilidadMS.config;
-
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
@@ -7,7 +6,6 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
-
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
