018bc0755fe4a4038c8c362241709162d0d10c96
CALEB <go.gallardoe@duocuc.cl>
2026-07-12 21:49:32 -0400

    Update VehiculoDTO.java

--- disponibilidadMS/disponibilidadMS/src/main/java/cl/duoc/disponibilidadMS/dto/VehiculoDTO.java
+++ disponibilidadMS/disponibilidadMS/src/main/java/cl/duoc/disponibilidadMS/dto/VehiculoDTO.java
@@ -1,12 +1,10 @@
 package cl.duoc.disponibilidadMS.dto;
-
 import lombok.*;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class VehiculoDTO {
-
     private Integer id;
     private String patente;
     private String marca;
