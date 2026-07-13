86dacc9f3c428fe153bf6f789b219e1ab6c85772
CALEB <go.gallardoe@duocuc.cl>
2026-07-12 21:22:50 -0400

    upgrate CategoriaVehiculo

--- vehiculoMS/vehiculoMS/src/main/java/cl/duoc/vehiculoMS/model/CategoriaVehiculo.java
+++ vehiculoMS/vehiculoMS/src/main/java/cl/duoc/vehiculoMS/model/CategoriaVehiculo.java
@@ -1,5 +1,4 @@
 package cl.duoc.vehiculoMS.model;
-
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -13,7 +12,6 @@ public class CategoriaVehiculo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-
     @Column(nullable = false, unique = true)
     private String nombre;
 }
