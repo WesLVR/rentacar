3c00974601dc6e2c9752d7e8f3420fa18467ffe8
CALEB <go.gallardoe@duocuc.cl>
2026-07-12 21:51:33 -0400

    upgrate
    Removed unnecessary blank lines and added a newline at the end of the file.

--- disponibilidadMS/disponibilidadMS/src/main/java/cl/duoc/disponibilidadMS/repository/DisponibilidadRepository.java
+++ disponibilidadMS/disponibilidadMS/src/main/java/cl/duoc/disponibilidadMS/repository/DisponibilidadRepository.java
@@ -1,11 +1,8 @@
 package cl.duoc.disponibilidadMS.repository;
-
 import java.time.LocalDate;
 import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import cl.duoc.disponibilidadMS.model.Disponibilidad;
 
 @Repository
