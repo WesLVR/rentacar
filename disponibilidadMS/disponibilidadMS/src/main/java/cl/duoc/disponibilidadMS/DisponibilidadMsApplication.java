34d7ff18ff5c834223db3de477737afee21c98b8
CALEB <go.gallardoe@duocuc.cl>
2026-07-12 21:55:22 -0400

    upgrate

--- disponibilidadMS/disponibilidadMS/src/main/java/cl/duoc/disponibilidadMS/DisponibilidadMsApplication.java
+++ disponibilidadMS/disponibilidadMS/src/main/java/cl/duoc/disponibilidadMS/DisponibilidadMsApplication.java
@@ -1,5 +1,4 @@
 package cl.duoc.disponibilidadMS;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -7,7 +6,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients
 @SpringBootApplication
 public class DisponibilidadMsApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DisponibilidadMsApplication.class, args);
     }
