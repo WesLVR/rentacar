438717fabd725bf2725f78a111fc9a61d3fe3b65
CALEB <go.gallardoe@duocuc.cl>
2026-07-12 21:23:37 -0400

    upgrate Vehiculo.java

--- vehiculoMS/vehiculoMS/src/main/java/cl/duoc/vehiculoMS/model/Vehiculo.java
+++ vehiculoMS/vehiculoMS/src/main/java/cl/duoc/vehiculoMS/model/Vehiculo.java
@@ -1,5 +1,4 @@
 package cl.duoc.vehiculoMS.model;
-
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -13,24 +12,17 @@ public class Vehiculo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-
     @Column(nullable = false, unique = true)
     private String patente;
-
     @Column(nullable = false)
     private String marca;
-
     @Column(nullable = false)
     private String modelo;
-
     @Column(nullable = false)
     private Integer anio;
-
     @Column(nullable = false)
     private Double precioPorDia;
-
     @ManyToOne
     @JoinColumn(name = "categoria_id", nullable = false)
     private CategoriaVehiculo categoria;
 }
-
