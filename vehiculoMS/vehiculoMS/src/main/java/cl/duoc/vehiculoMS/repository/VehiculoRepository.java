eef6649ac8c3148db06f2ddd3a39ca332ee81f3d
CALEB <go.gallardoe@duocuc.cl>
2026-07-12 21:25:35 -0400

    Upgrate

--- vehiculoMS/vehiculoMS/src/main/java/cl/duoc/vehiculoMS/repository/VehiculoRepository.java
+++ vehiculoMS/vehiculoMS/src/main/java/cl/duoc/vehiculoMS/repository/VehiculoRepository.java
@@ -1,17 +1,12 @@
 package cl.duoc.vehiculoMS.repository;
-
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import cl.duoc.vehiculoMS.model.Vehiculo;
 
 @Repository
 public interface VehiculoRepository extends JpaRepository<Vehiculo, Integer> {
-
     Optional<Vehiculo> findByPatente(String patente);
-
     List<Vehiculo> findByCategoriaId(Integer categoriaId);
 }
